1a6eac6e3cc5a9da17501920d1d4d0add809bf06
pfeningviola <pfeningviola@gmail.com>
2019-08-29 16:10:39 -0300

    feat(banner): create banner component and add bootstrap links to index.html

--- timogf-angular-frontend/src/app/app.module.ts
+++ timogf-angular-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainComponent } from './_components/main/main.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BannerComponent } from './_components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
