33a8c96f8aa0086cbff54d184c8b946fc5a468f3
pfeningviola <pfeningviola@gmail.com>
2019-08-30 11:18:35 -0300

    feat(BannerComponent): add the banner image to the banner.component.html and add background color to styles.css

--- timogf-angular-frontend/src/app/app-routing.module.ts
+++ timogf-angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+//With the routing module we will have the option to make
+//other paths with other routes in the future
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './_components/main/main.component';
 
+//in this moment we have only the MainComponent view,
+//all paths will redirect to it
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: '**', redirectTo: '' }
