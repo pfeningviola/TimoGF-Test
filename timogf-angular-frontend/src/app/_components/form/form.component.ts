5192fbc203a394c0d08b28d7ab81cadbcb5059b4
pfeningviola <pfeningviola@gmail.com>
2019-08-30 15:21:49 -0300

    feat(FormComponent): add onSubmit() method to form.component.ts to be able to show the submitted data

--- timogf-angular-frontend/src/app/_components/form/form.component.ts
+++ timogf-angular-frontend/src/app/_components/form/form.component.ts
@@ -9,12 +9,22 @@ import { Component, OnInit } from '@angular/core';
 
 export class FormComponent implements OnInit {
   user: User = new User();
+  submittedName: string;
+  submittedText: string;
+  message: string;
   submitted = false;
- 
+  validData = false;
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    this.validData = true;
+    this.submittedName = this.user.name;
+    this.submittedText = this.user.text;
   }
 
 
