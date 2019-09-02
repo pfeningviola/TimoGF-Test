f84fd40068776c5cc481fd98ee3dfc7e146584e9
pfeningviola <pfeningviola@gmail.com>
2019-09-02 10:56:30 -0300

    feat(ValidationService): create ValidationService and create methods to validate the user input and to return how many parts the username has

--- timogf-java-backend/src/main/java/com/timogf/test/test/controllers/ValidationController.java
+++ timogf-java-backend/src/main/java/com/timogf/test/test/controllers/ValidationController.java
@@ -2,6 +2,8 @@ package com.timogf.test.test.controllers;
 
 import com.timogf.test.test.models.UserInputDTO;
 import com.timogf.test.test.models.ValidationResponseDTO;
+import com.timogf.test.test.services.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,10 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ValidationController {
+    private ValidationService validationService;
+
+    @Autowired
+    public ValidationController(ValidationService validationService){
+        this.validationService = validationService;
+    }
 
     @PostMapping("/validation")
     public ResponseEntity<Object> validateUserInput(@RequestBody UserInputDTO userInput) {
 
-        return ResponseEntity.status(200).body( new ValidationResponseDTO);
+        return ResponseEntity.status(200).body( new ValidationResponseDTO());
     }
 }
---
+++ timogf-java-backend/src/main/java/com/timogf/test/test/services/ValidationService.java
@@ -0,0 +1,20 @@
+package com.timogf.test.test.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ValidationService {
+
+    public boolean isCompleteName(String name){
+        String[] partsOfName = name.trim().split(" ");
+        return partsOfName.length > 1;
+    }
+
+    public boolean isAnyCharacterInTheText(String text){
+        return text.trim().length() >= 1;
+    }
+
+    public int numberOfPartsOfName(String name){
+        return name.trim().split(" ").length;
+    }
+}
