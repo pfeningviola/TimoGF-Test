d69b0ec1ca637aeee85e06bdd5eeca4db385f062
pfeningviola <pfeningviola@gmail.com>
2019-09-02 11:06:42 -0300

    feat(ValidationController): add /validation endpoint to validate the user input

--- timogf-java-backend/src/main/java/com/timogf/test/test/controllers/ValidationController.java
+++ timogf-java-backend/src/main/java/com/timogf/test/test/controllers/ValidationController.java
@@ -1,5 +1,6 @@
 package com.timogf.test.test.controllers;
 
+import com.timogf.test.test.models.ErrorMessage;
 import com.timogf.test.test.models.UserInputDTO;
 import com.timogf.test.test.models.ValidationResponseDTO;
 import com.timogf.test.test.services.ValidationService;
@@ -20,7 +21,15 @@ public class ValidationController {
 
     @PostMapping("/validation")
     public ResponseEntity<Object> validateUserInput(@RequestBody UserInputDTO userInput) {
+        if (!validationService.isCompleteName(userInput.getName())){
+            return ResponseEntity.status(400).body(new ErrorMessage("The given name is not a complete name."));
+        }
 
-        return ResponseEntity.status(200).body( new ValidationResponseDTO());
+        if (!validationService.isAnyCharacterInTheText(userInput.getText())){
+            return ResponseEntity.status(400).body(new ErrorMessage("The text must contain at least one non-space character."));
+        }
+
+        int numberOfPartsOfName = validationService.numberOfPartsOfName(userInput.getName());
+        return ResponseEntity.status(200).body( new ValidationResponseDTO(userInput, numberOfPartsOfName));
     }
 }
