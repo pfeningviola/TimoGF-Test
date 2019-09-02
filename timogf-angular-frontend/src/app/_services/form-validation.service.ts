7fdc85f64f248b17f9fc9cdd9539ee58d05a0c39
pfeningviola <pfeningviola@gmail.com>
2019-09-02 16:01:02 -0300

    fix(FormValidationService): fix isSubmittedDataValid() method to show the 'All field are requiered' error message, if in the input fields are filled only with whitespaces

--- timogf-angular-frontend/src/app/_services/form-validation.service.ts
+++ timogf-angular-frontend/src/app/_services/form-validation.service.ts
@@ -21,7 +21,7 @@ export class FormValidationService {
 
   isSubmittedDataValid(name: string, text: string) {
     // check if all field are filled
-    if (name === null || text === null || name === '' || text === '') {
+    if (name === null || text === null || name.trim() === '' || text.trim() === '') {
       return new DataValidation(false, 'All field are required.');
     }
     const partsOfName: string[] = name.trim().split(' ');
